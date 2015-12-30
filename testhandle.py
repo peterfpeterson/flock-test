8da1e40847bffa5d59b167f319beb6c2d6c381bc
Pete Peterson <petersonpf@ornl.gov>
2015-12-30 11:14:25 -0500

    Combined into single 'with' command

--- testhandle.py
+++ testhandle.py
@@ -5,9 +5,7 @@ import time
 # explanation of 'with' http://effbot.org/zone/python-with-statement.htm
 with open('tempfile', 'w') as f:
     # does not work without a proper file handle
-    blocking_lock = flock.Flock(f, flock.LOCK_EX)
-
-    with blocking_lock:
+    with flock.Flock(f, flock.LOCK_EX): # blocking lock
         print "have lock"
         time.sleep(10)
 
